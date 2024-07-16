7a25c6f2e0995e3bca78e5803a32a32df6310242
s <tylereunholee010203@gmail.com>
2024-07-16 11:57:18 +0900

    프로젝트 세팅

---
+++ JBT_DSM_iOS/JBT_DSM_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JBT_DSM_iOS
+//
+//  Created by 이은호 on 7/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
